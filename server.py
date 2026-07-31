19db1483def4e13b05de810df392f6df19112067
the-man-with-no-name <64222379+the-man-with-no-name@users.noreply.github.com>
2026-07-31 10:58:50 -0700

    Remove escaping characters.

--- server.py
+++ server.py
@@ -318,7 +318,7 @@ async def vectors_time_list(
     if site_coord is not None:
         query_params["SITE_COORD"] = format_to_single_quote_string(format_to_comma_sep_string(site_coord))
     if time_list is not None:
-        query_params["TLIST"] = format_escape_char_url(format_to_space_sep_string(map(format_to_single_quote_string, map(format_to_custom_datetime, time_list))))
+        query_params["TLIST"] = format_to_space_sep_string(map(format_to_single_quote_string, map(format_to_custom_datetime, time_list))) #format_escape_char_url(format_to_space_sep_string(map(format_to_single_quote_string, map(format_to_custom_datetime, time_list))))
 
     async with httpx.AsyncClient() as client:
         try:
